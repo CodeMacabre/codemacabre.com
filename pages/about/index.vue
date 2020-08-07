8ef58f0f981ddd70dcd13c10674b14ab9af8722e
Myles Lewando <49149591+codemacabre@users.noreply.github.com>
2020-08-07 16:23:43 +0100

    💬 Update 'about' copy

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -3,60 +3,34 @@
     <section>
       <h2 id="me">About Me</h2>
       <p>
-        I am Myles Lewando
-        <span class="aside">(pronounced 'le-van-doe')</span>, a web developer
-        and designer specialising in cartography, web-GIS and data
-        visualisation.
+        Hello, I'm Myles Lewando
+        <span class="aside">(pronounced 'le-van-doe')</span>, a web developer and data artist.
       </p>
       <p>
         By day, I work as a front-end web developer at
         <a href="https://www.adgistics.com/">Adgistics</a>. By night, I work
-        on web and map projects ranging from personal, to humanitarian and
-        non-profit, to commercial.
+        on web, map and datavis projects ranging from personal, to humanitarian and non-profit, to commercial.
       </p>
       <p>
-        I am an open-source and open-data advocate and I value web
-        accessibility, inclusive design and self-directed digital learning. I
-        also have an affinity for space, ecology, insects, and the macabre
-        <span
-          class="aside"
-        >(hence the pseudonym / company name 'CodeMacabre')</span>.
+        I am an (ethical) open-source and open-data advocate and I value web accessibility, inclusive design and self-directed digital learning. I also have an affinity for space, ecology, insects, and the macabre
+        <span class="aside">(hence the alias 'CodeMacabre')</span>.
       </p>
-      <p>
-        I'd love to help you with your map or web project, particularly if
-        it's open source or aligned with my interests and values. Get in
-        touch!
-      </p>
+      <p>I'd love to help you with your map or web project, particularly if it's open source or aligned with my interests and values. Get in touch!</p>
     </section>
 
     <section>
       <h2 id="codemacabre">About CodeMacabre</h2>
       <p>
-        Being fascinated by medical history, mortality and art, I naturally
-        veer towards darker themes. One of my favourite series of artworks are
-        Hans Holbein's
-        <a
-          href="https://archive.org/details/lessimulachreshi1538holb/"
-        >Danse Macabre</a>
-        woodcuts. The meaning behind these prints aligns surprisingly closely
-        with ideas of inclusivity and equality and, as a developer, it felt
-        like an obvious progression to consider a Code Macabre, a modern-day
-        progression of the
+        Being fascinated by medical history, mortality and art, I naturally veer towards darker themes. One of my favourite series of artworks are Hans Holbein's
+        <a href="https://archive.org/details/lessimulachreshi1538holb/">Danse Macabre</a> woodcuts. The meaning behind these prints aligns surprisingly closely with ideas of inclusivity and equality and, as a developer, it felt
+        like an obvious progression to consider a Code Macabre, a modern-day progression of the
         <em>danse</em>.
       </p>
       <p>
-        That inclusivity and equality is a fundamental right of everyone,
-        regardless of our gender, sexuality, race, mental or physical
-        ableness, wealth, etc. The code that powers the web and beyond should
-        respect those rights; the
-        <em>danse macabre</em>'s idea of 'equality
-        in mortality' and respecting others accordingly rings true today
-        across our physical and digital lives; and beyond.
+        That inclusivity and equality is a fundamental right of everyone, regardless of our gender, sexuality, race, mental or physical ableness, wealth, etc. I beleive the code that powers the web should respect those rights; the
+        <em>danse macabre</em>'s idea of 'equality in mortality' and respecting others accordingly rings true today across our physical and digital lives.
       </p>
-      <p>
-        CodeMacabre therefore is my mission; I intend to work towards these
-        tenets with everything I do, starting with the web.
-      </p>
+      <p>CodeMacabre therefore is my mission; I intend to work towards these tenets with everything I do, starting with the web.</p>
     </section>
   </main>
 </template>
