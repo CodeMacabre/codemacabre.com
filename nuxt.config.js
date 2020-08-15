e7285e0b48c81da4dd5671224ed5ab4e3f7cba0e
Myles Lewando <49149591+codemacabre@users.noreply.github.com>
2020-08-15 18:49:40 +0100

    🔧 Configure FontAwesome icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,11 @@
 export default {
+  buildModules: [
+    ['@nuxtjs/fontawesome', {
+      icons: {
+        solid: ['faEnvelopeOpenText', 'faFolder', 'faFolderOpen', 'faPenNib', 'faUndoAlt'],
+        brands: ['faGithub', 'faLinkedin', 'faTwitter']
+      }
+    }]
+  ],
   css: ['~/assets/css/main.css']
 }
